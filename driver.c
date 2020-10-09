843cc1a6459aface7313167b8f35ec503b461787
antoine_danquigny <danquigny16@gmail.com>
2020-10-09 19:12:52 +0200

    init et modif d'un elt pour matrice et vecteur + test

--- driver.c
+++ driver.c
@@ -3,11 +3,84 @@
 
 #include "util.h"
 
-// gcc -Wall -Wextra -std=c99 util.h util.c driver.c -o test
-int main (int argc, char ** argv){
-  int * a = matrice(3, 4);
+
+////////////////////////////////////////////////////////////////////////////////
+// Tests
+
+/**
+Fait les tests relatifs aux allocation et libération mémoire de matrices et de vecteurs
+*/
+void test_alloc_et_free(){
+  // Nom du test
+  printf("*************** TEST ALLOC ET FREE ***************\n");
+
+  // Matrice
+  printf("\n----- Matrice -----\n\n");
+
+  double * a = matrice(3, 4);
   affiche(3, 4, a, 3, stdout);
-  free(a);
+  free_matrice(a);
+
+  // Vecteur
+  printf("\n----- Vecteur -----\n\n");
+
+
+  double * v = vecteur(5);
+  affiche_vecteur(5, v, 1, stdout);
+  free_vecteur(v);
+
+  // Fin du test
+  printf("\n");
+}
+
+/**
+Fait les tests relatifs aux initialisations de matrices et de vecteurs
+*/
+void test_initialisation(){
+  // Nom du test
+  printf("*************** TEST INITIALISATION ***************\n");
+
+  // Matrice
+  printf("\n----- Matrice -----\n\n");
+
+  double * a = matrice(3, 4);
+  init_matrice(3, 4, 3, a);
+  affiche(3, 4, a, 3, stdout);
+
+  printf("\n----- On met 42.0 à la position (1,3) -----\n\n");
+
+  fixer_elt_matrice(1, 3, 3, a, 42.0);
+  affiche(3, 4, a, 3, stdout);
+
+  free_matrice(a);
+
+  // Vecteur
+  printf("\n----- Vecteur -----\n\n");
+
+
+  double * v = vecteur(5);
+  init_vecteur(5, 1, v);
+  affiche_vecteur(5, v, 1, stdout);
+
+  printf("\n----- On met 42.0 à la position 2 -----\n\n");
+
+  fixer_elt_vecteur(2, 1, v, 42.0);
+  affiche_vecteur(5, v, 1, stdout);
+
+  free_vecteur(v);
+
+  // Fin du test
+  printf("\n");
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Main
+
+// gcc -Wall -Wextra -std=c99 cblas.h util.h util.c driver.c -o test
+int main (/*int argc, char ** argv*/){
+  test_alloc_et_free();
+  test_initialisation();
 
   return 0;
 }
--- util.c
+++ util.c
@@ -1,5 +1,9 @@
 #include "util.h"
 
+
+////////////////////////////////////////////////////////////////////////////////
+// Affichage
+
 /**
 Affiche la matrice a
 @param m : hauteur
@@ -8,12 +12,12 @@ Affiche la matrice a
 @param lda : leading dimension (si a est une sous-matrice d'une matrice M, lda est la hauteur de M (car on stocke par colonne))
 @param flux : flux de sortie
 */
-void affiche(int m, int n, int * a, int lda, FILE * flux){
+void affiche(int m, int n, double *a, int lda, FILE * flux){
   // ligne
   for (int i = 0; i < m; i++){
     // colonne
     for (int j = 0; j < n; j++){
-      fprintf(flux, "%d", a[j * lda + i]);
+      fprintf(flux, "%f  ", a[i + j * lda]);
     }
     // fin de la ligne
     fprintf(flux, "\n");
@@ -21,20 +25,135 @@ void affiche(int m, int n, int * a, int lda, FILE * flux){
 }
 
 /**
+Affiche le vecteur v
+@param n : taille
+@param v : vecteur
+@param lda : leading dimension (si v est un vecteur d'une ligne matrice M, lda est la hauteur de M (car on stocke par colonne))
+@param flux : flux de sortie
+*/
+void affiche_vecteur(int n, double *v, int lda, FILE * flux){
+  for (int i = 0; i < n; i++){
+    fprintf(flux, "%f", v[i * lda]);
+    fprintf(flux, "\n");
+  }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Allocation et libération des matrices et des vecteurs
+
+/**
 Alloue la mémoire pour une matrice de dim m*n, et renvoie un pointeur vers cette matrice
 @param m : hauteur
 @param n : largeur
+@return Un pointeur sur la matrice
 */
-int * matrice(int m, int n){
-  int * mat = malloc(m * n * sizeof(int));
+double * matrice(int m, int n){
+  double * mat = malloc(m * n * sizeof(double));
   return mat;
 }
 
 /**
 Alloue la mémoire pour un vecteur de dim n, et renvoie un pointeur vers ce vecteur
-@param n : hauteur
+@param m : hauteur
+@return Un pointeur sur le vecteur
 */
-int * vecteur(int n){
-  int * vec = malloc(n * sizeof(int));
+double * vecteur(int n){
+  double * vec = malloc(n * sizeof(double));
   return vec;
+}
+
+/**
+Libère la mémoire pour la matrice "a"
+@param a La matrice
+*/
+void free_matrice(double *a){
+  free(a);
+}
+
+/**
+Libère la mémoire pour le vecteur "a"
+@param a La matrice
+*/
+void free_vecteur(double *v){
+  free(v);
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Initialisation et modification des matrices et des vecteurs
+
+/**
+Fixe la valeur de l'élément en position (m, n) à "val"
+@param m : ligne
+@param n : colonne
+@param a : matrice
+@param val : nouvel valeur de l'élément en position (m, n)
+*/
+void fixer_elt_matrice(int m, int n, int lda, double *a, double val){
+  a[m + n * lda] = val;
+}
+
+/**
+Fixe la valeur de l'élément en position "n" à "val"
+@param n : numéro de l'élément à modifier
+@param v : matrice
+@param val : nouvel valeur de l'élément en position (m, n)
+*/
+void fixer_elt_vecteur(int n, int lda, double *v, double val){
+  v[n * lda] = val;
+}
+
+/**
+initialise la matrice en allant de 1.0 à m*n, de gauche à droite puis de haut en bas
+@param m : hauteur
+@param n : largeur
+@param a : matrice
+*/
+void init_matrice(int m, int n, int lda, double *a){
+  double val = 1.0;
+  // ligne
+  for (int i = 0; i < m; i++){
+    // colonne
+    for (int j = 0; j < n; j++){
+      a[i + j * lda] = val;
+      val++;
+    }
+  }
+}
+
+/**
+initialise le vecteur en allant de 1.0 à n, de haut en bas
+@param n : taille
+@param v : vecteur
+*/
+void init_vecteur(int n, int lda, double *v){
+  double val = 1.0;
+  for (int i = 0; i < n; i++){
+    v[i * lda] = val;
+    val++;
+  }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Opération sur les matrices et les vecteurs
+
+/**
+Effectue le produit scalaire entre les vecteurs "X" et "Y"
+@param N Nombre d'éléments des vecteurs "X" et "Y"
+@param X Vecteur "X"
+@param incX Incrément entre chaque élément de "X"
+@param Y Vecteur "Y"
+@param incX Incrément entre chaque élément de "Y"
+@return Le résultat du produit scalaire
+*/
+double my_ddot(const int N, const double *X, const int incX, const double *Y, const int incY){
+  int res = 0;
+
+  for (int i = 0; i < N; i++){
+    res += X[i*incX] * Y[i*incY];
+  }
+
+  return res;
 }
